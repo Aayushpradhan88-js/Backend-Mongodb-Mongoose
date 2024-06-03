88262966ab4cd2a727ed6dbcbe93f1c6b79b9fa5
Aayush Pradhan <154524186+Aayushpradhan88-js@users.noreply.github.com>
2024-06-03 23:02:29 +0000

    writing databse codes

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const app = express();
 const userSchema = require('./userSchema');
-
-userSchema.create = 
+const { name } = require('ejs');
+// const { name } = require('ejs');
 
 app.get("/", (req, res) => {
     res.send("Hey my name is aayush pradhan");
+})
+
+app.get("/create", async (req, res) => {
+    let create = await userSchema.create({
+        username: "Aayush",
+        name: "Aayush Pradhan",
+        address: "Itahari",
+        age: 19
+    })
+
+    res.send(create)
+})
+
+app.get("/update", async (req, res) =>{
+    let updateUser = await userSchema.findOneUpdate({username:"aayush"}, {name: "aayush pradhan"}, {address:"itahari"}, {age:19});
+    res.send(updateUser);
 })
 
 const port = 3000;
